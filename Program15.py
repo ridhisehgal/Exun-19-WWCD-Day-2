0c81226bcd38235bc52e7d7f63a85c33f11f197b
ridhisehgal <56750672+ridhisehgal@users.noreply.github.com>
2019-10-20 09:59:06 +0530

    Update Program15.py

--- Program15.py
+++ Program15.py
@@ -1,4 +1,5 @@
 def is_abundant(n):
-    s = sum([fctr for fctr in range(1, n + 1) if n / fctr == 0])
+    s = sum([for fctr in range(1, n + 1):
+                if n / fctr == 0])
     return s > n
 print(is_abundant(12))
