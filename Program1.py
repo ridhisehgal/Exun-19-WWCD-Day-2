a9b434130914fc8717b2f0a0d8d5405a4819350e
ridhisehgal <56750672+ridhisehgal@users.noreply.github.com>
2019-10-20 10:20:26 +0530

    Update Program1.py

--- Program1.py
+++ Program1.py
@@ -2,7 +2,7 @@
 # Complexity: H
 def tribo(n):
         if n == 1:
-        return 0;
+                return 0;
         if n == 2:
         return 0;
         return tribo(n - 1) + tribo(n - 2) + tribo(n - 3);
