ee2a5ec3efe4f93a51b2e67bafc447cd8e9b8562
ridhisehgal <56750672+ridhisehgal@users.noreply.github.com>
2019-10-20 10:47:19 +0530

    Update Program16.py

--- Program16.py
+++ Program16.py
@@ -19,7 +19,7 @@ def orgate(a, b):
 		return False
 def NOR(a, b): 
     if(a == 0) and (b == 0): 
-        return 0
+        return 1
     elif(a == 0) and (b == 1): 
         return 1
     elif(a == 1) and (b == 0): 
